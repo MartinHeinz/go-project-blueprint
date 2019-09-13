f22616b0a83760721a3c4b7c651ea19f2d096fae
Martin Heinz <martin.heinz1@ibm.com>
2019-09-13 15:29:59 +0200

    Revert example test to first version.

--- cmd/blueprint/main_test.go
+++ cmd/blueprint/main_test.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"github.com/MartinHeinz/go-project-blueprint/cmd/blueprint/config"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 // Example test to show usage of `make test`
 func TestDummy(t *testing.T) {
-	assert.Equal(t, config.Config.ConfigVar, "Dummy Value")
+	assert.Equal(t, 1, 1)
 }
